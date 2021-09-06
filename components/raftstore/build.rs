746f98d96b0317a99264d0e9273c5807de865017
CalvinNeo <calvinneo1995@gmail.com>
2021-09-06 21:15:27 +0800

    polish

--- components/raftstore/build.rs
+++ components/raftstore/build.rs
@@ -1,9 +1,7 @@
-
 fn main() {
     println!(
-    "cargo:rerun-if-changed={}",
-    "components/raftstore/src/engine_store_ffi/interfaces.rs"
+        "cargo:rerun-if-changed={}",
+        "components/raftstore/src/engine_store_ffi/interfaces.rs"
     );
-    std::process::Command::new("touch").args(&["./aaa.txt"]).output().expect("sh exec error!");
     gen_proxy_ffi::gen_ffi_code();
 }
