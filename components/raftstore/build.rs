3a88fb99c86e5ae8762be1097128eeab2722b0ac
CalvinNeo <calvinneo1995@gmail.com>
2021-09-07 10:38:54 +0800

    add .gitignore

--- components/raftstore/build.rs
+++ components/raftstore/build.rs
@@ -3,5 +3,9 @@ fn main() {
         "cargo:rerun-if-changed={}",
         "components/raftstore/src/engine_store_ffi/interfaces.rs"
     );
+    println!(
+        "cargo:rerun-if-changed={}",
+        "raftstore-proxy/ffi/src/RaftStoreProxyFFI"
+    );
     gen_proxy_ffi::gen_ffi_code();
 }
