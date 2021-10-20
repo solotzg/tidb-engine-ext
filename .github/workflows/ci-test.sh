0cb5d324a5cf2a6b2b3bb55e792427e5afa2c8dd
CalvinNeo <calvinneo1995@gmail.com>
2021-10-20 22:19:50 +0800

    Add test
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

--- .github/workflows/ci-test.sh
+++ .github/workflows/ci-test.sh
@@ -54,7 +54,7 @@ cargo test --package tests --test integrations -- raftstore::test_region_info_ac
 cargo test --package tests --test integrations -- raftstore::test_transfer_leader && \
 cargo test --package tests --test integrations -- raftstore::test_single && \
 # Sometimes fails
-#cargo test --package tests --test integrations -- raftstore::test_merge && \
+cargo test --package tests --test integrations -- raftstore::test_merge && \
 
 if [ ${GENERATE_COV:-0} -ne 0 ]; then
   grcov . --binary-path target/debug/ . -t html --branch --ignore-not-existing -o ./coverage/
