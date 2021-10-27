689c20203646f7d3021bd4ba6ccea2ebaca6512a
CalvinNeo <calvinneo1995@gmail.com>
2021-10-27 11:02:25 +0800

    Lengthen timeout

--- components/test_raftstore/src/pd.rs
+++ components/test_raftstore/src/pd.rs
@@ -1070,7 +1070,14 @@ impl TestPdClient {
     pub fn must_merge(&self, from: u64, target: u64) {
         self.merge_region(from, target);
 
-        self.check_merged_timeout(from, Duration::from_secs(15));
+        self.check_merged_timeout(
+            from,
+            Duration::from_secs(if cfg!(feature = "test-raftstore-proxy") {
+                25
+            } else {
+                15
+            }),
+        );
     }
 
     pub fn check_merged(&self, from: u64) -> bool {
