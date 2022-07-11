573efc6d3d155a9a01da003e70f111485becf2bc
CalvinNeo <calvinneo1995@gmail.com>
2022-07-11 18:17:29 +0800

    add disclaimer
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

--- components/proxy_server/src/config.rs
+++ components/proxy_server/src/config.rs
@@ -1,3 +1,5 @@
+// Copyright 2022 TiKV Project Authors. Licensed under Apache-2.0.
+
 use std::{
     collections::{hash_map::RandomState, HashSet},
     iter::FromIterator,
--- components/raftstore/src/engine_store_ffi/mod.rs
+++ components/raftstore/src/engine_store_ffi/mod.rs
@@ -1,3 +1,5 @@
+// Copyright 2022 TiKV Project Authors. Licensed under Apache-2.0.
+
 #[allow(dead_code)]
 pub mod interfaces;
 
