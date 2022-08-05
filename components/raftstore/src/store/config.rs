0d0ae48a40902bc3d6a2b73ca69146abba9bbbf5
Calvin Neo <CalvinNeo@users.noreply.github.com>
2022-08-05 14:49:41 +0800

    updating proxy 6.1 for 2 bugfixs (#135)
    * raftstore: make `UNREACHABLE_BACKOFF` configurable (#13193)
    
    close tikv/tikv#13054
    
    make `UNREACHABLE_BACKOFF` configurable.
    
    Signed-off-by: 5kbpers <hustmh@gmail.com>
    
    * raftstore: use force_send to send ApplyRes (#13168) (#13171)
    
    close tikv/tikv#13160, ref tikv/tikv#13168
    
    Use force_send to send ApplyRes
    
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>
    Signed-off-by: 5kbpers <hustmh@gmail.com>
    
    Co-authored-by: 5kbpers <hustmh@gmail.com>
    
    * f
    
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>
    
    Co-authored-by: 5kbpers <hustmh@gmail.com>
    Co-authored-by: ti-srebot <66930949+ti-srebot@users.noreply.github.com>

--- components/raftstore/src/store/config.rs
+++ components/raftstore/src/store/config.rs
@@ -285,6 +285,8 @@ pub struct Config {
     pub reactive_memory_lock_tick_interval: ReadableDuration,
     /// Max tick count before reactivating in-memory pessimistic lock.
     pub reactive_memory_lock_timeout_tick: usize,
+
+    pub unreachable_backoff: ReadableDuration,
 }
 
 impl Default for Config {
@@ -378,6 +380,7 @@ impl Default for Config {
             report_min_resolved_ts_interval: ReadableDuration::millis(0),
             check_leader_lease_interval: ReadableDuration::secs(0),
             renew_leader_lease_advance_duration: ReadableDuration::secs(0),
+            unreachable_backoff: ReadableDuration::secs(10),
         }
     }
 }
