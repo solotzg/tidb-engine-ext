580cd11bf0d8a291a51d989c4e220f2f80eefe2f
CalvinNeo <calvinneo1995@gmail.com>
2022-10-28 23:19:09 +0800

    move proxy_tests out
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

--- ci_check.sh
+++ ci_check.sh
@@ -24,17 +24,17 @@ elif [[ $M == "testnew" ]]; then
     export ENGINE_LABEL_VALUE=tiflash
     export RUST_BACKTRACE=full
     # tests based on new-mock-engine-store, with compat for new proxy
-    cargo test --package tests --test proxy normal::store
-    cargo test --package tests --test proxy normal::region
-    cargo test --package tests --test proxy normal::config
-    cargo test --package tests --test proxy normal::write
-    cargo test --package tests --test proxy normal::ingest
-    cargo test --package tests --test proxy normal::snapshot
-    cargo test --package tests --test proxy normal::restart
-    cargo test --package tests --test proxy normal::persist
-    cargo test --package tests --test proxy server_cluster_test
+    cargo test --package proxy_tests --test proxy normal::store
+    cargo test --package proxy_tests --test proxy normal::region
+    cargo test --package proxy_tests --test proxy normal::config
+    cargo test --package proxy_tests --test proxy normal::write
+    cargo test --package proxy_tests --test proxy normal::ingest
+    cargo test --package proxy_tests --test proxy normal::snapshot
+    cargo test --package proxy_tests --test proxy normal::restart
+    cargo test --package proxy_tests --test proxy normal::persist
+    cargo test --package proxy_tests --test proxy server_cluster_test
     # tests based on new-mock-engine-store, for some tests not available for new proxy
-    cargo test --package tests --test proxy proxy
+    cargo test --package proxy_tests --test proxy proxy
 elif [[ $M == "debug" ]]; then
     # export RUSTC_WRAPPER=~/.cargo/bin/sccache
     export ENGINE_LABEL_VALUE=tiflash
