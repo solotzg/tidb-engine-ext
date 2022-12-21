3ad722a1dcc4ee01c99d33b9f24574f1047db0aa
lidezhu <lidezhu@pingcap.com>
2022-12-21 16:34:23 +0800

    fix test

--- engine_tiflash/src/write_batch.rs
+++ engine_tiflash/src/write_batch.rs
@@ -21,6 +21,8 @@ impl WriteBatchExt for RocksEngine {
     fn write_batch(&self) -> RocksWriteBatchVec {
         RocksWriteBatchVec::new(
             Arc::clone(self.as_inner()),
+            self.ffi_hub.clone(),
+            self.ffi_hub.as_ref().unwrap().create_write_batch(),
             WRITE_BATCH_LIMIT,
             1,
             self.support_multi_batch_write(),
@@ -28,7 +30,11 @@ impl WriteBatchExt for RocksEngine {
     }
 
     fn write_batch_with_cap(&self, cap: usize) -> RocksWriteBatchVec {
-        RocksWriteBatchVec::with_unit_capacity(self, cap)
+        RocksWriteBatchVec::with_unit_capacity(
+            self,
+            self.ffi_hub.as_ref().unwrap().create_write_batch(),
+            cap,
+        )
     }
 }
 
@@ -289,7 +295,14 @@ mod tests {
 
         assert!(v.is_some());
         assert_eq!(v.unwrap(), b"bbb");
-        let mut wb = RocksWriteBatchVec::with_unit_capacity(&engine, 1024);
+        let mut wb = RocksWriteBatchVec::with_unit_capacity(
+            &engine,
+            RawPSWriteBatchWrapper {
+                ptr: std::ptr::null_mut(),
+                type_: 0,
+            },
+            1024,
+        );
         for _i in 0..RocksEngine::WRITE_BATCH_MAX_KEYS {
             wb.put(b"aaa", b"bbb").unwrap();
         }
@@ -329,7 +342,14 @@ mod tests {
         assert!(!wb.should_write_to_engine());
         wb.put(b"aaa", b"bbb").unwrap();
         assert!(wb.should_write_to_engine());
-        let mut wb = RocksWriteBatchVec::with_unit_capacity(&engine, 1024);
+        let mut wb = RocksWriteBatchVec::with_unit_capacity(
+            &engine,
+            RawPSWriteBatchWrapper {
+                ptr: std::ptr::null_mut(),
+                type_: 0,
+            },
+            1024,
+        );
         for _i in 0..WRITE_BATCH_MAX_BATCH * WRITE_BATCH_LIMIT {
             wb.put(b"aaa", b"bbb").unwrap();
         }
