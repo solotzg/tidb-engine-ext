49d4b7b9334b44ba17b0138ddfb5b8a2daf2557e
CalvinNeo <calvinneo1995@gmail.com>
2023-02-10 11:13:50 +0800

    fix compile errors
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

--- engine_store_ffi/src/ffi/mod.rs
+++ engine_store_ffi/src/ffi/mod.rs
@@ -3,11 +3,10 @@
 pub mod raftstore_proxy_engine;
 
 pub use engine_tiflash::EngineStoreConfig;
-
 pub use proxy_ffi::{
     basic_ffi_impls::*, domain_impls::*, encryption_impls::*, engine_store_helper_impls::*,
-    interfaces::root::DB as interfaces_ffi, lock_cf_reader::*, raftstore_proxy::*,
-    raftstore_proxy_helper_impls::*, sst_reader_impls::*, raftstore_proxy::*, raftstore_proxy,
+    interfaces::root::DB as interfaces_ffi, lock_cf_reader::*, raftstore_proxy, raftstore_proxy::*,
+    raftstore_proxy_helper_impls::*, read_index_helper, sst_reader_impls::*,
 };
 
-pub use self::{raftstore_proxy_engine::*, };
+pub use self::raftstore_proxy_engine::*;
