116a127211560da3dc5d22aac60f754944366145
lidezhu <lidezhu@pingcap.com>
2023-02-21 13:21:18 +0800

    add prefix to ffi

--- proxy_ffi/src/interfaces.rs
+++ proxy_ffi/src/interfaces.rs
@@ -420,59 +420,59 @@ pub mod root {
                     arg5: u64,
                 ) -> u8,
             >,
-            pub fn_create_write_batch: ::std::option::Option<
+            pub fn_ps_create_write_batch: ::std::option::Option<
                 unsafe extern "C" fn(
                     arg1: *const root::DB::EngineStoreServerWrap,
                 ) -> root::DB::RawCppPtr,
             >,
-            pub fn_wb_put_page: ::std::option::Option<
+            pub fn_ps_wb_put_page: ::std::option::Option<
                 unsafe extern "C" fn(
                     arg1: root::DB::RawVoidPtr,
                     arg2: root::DB::BaseBuffView,
                     arg3: root::DB::BaseBuffView,
                 ),
             >,
-            pub fn_wb_del_page: ::std::option::Option<
+            pub fn_ps_wb_del_page: ::std::option::Option<
                 unsafe extern "C" fn(arg1: root::DB::RawVoidPtr, arg2: root::DB::BaseBuffView),
             >,
-            pub fn_get_wb_size:
+            pub fn_ps_get_wb_size:
                 ::std::option::Option<unsafe extern "C" fn(arg1: root::DB::RawVoidPtr) -> u64>,
-            pub fn_is_wb_empty:
+            pub fn_ps_is_wb_empty:
                 ::std::option::Option<unsafe extern "C" fn(arg1: root::DB::RawVoidPtr) -> u8>,
-            pub fn_handle_merge_wb: ::std::option::Option<
+            pub fn_ps_handle_merge_wb: ::std::option::Option<
                 unsafe extern "C" fn(arg1: root::DB::RawVoidPtr, arg2: root::DB::RawVoidPtr),
             >,
-            pub fn_handle_clear_wb:
+            pub fn_ps_handle_clear_wb:
                 ::std::option::Option<unsafe extern "C" fn(arg1: root::DB::RawVoidPtr)>,
-            pub fn_handle_consume_wb: ::std::option::Option<
+            pub fn_ps_handle_consume_wb: ::std::option::Option<
                 unsafe extern "C" fn(
                     arg1: *const root::DB::EngineStoreServerWrap,
                     arg2: root::DB::RawVoidPtr,
                 ),
             >,
-            pub fn_handle_read_page: ::std::option::Option<
+            pub fn_ps_handle_read_page: ::std::option::Option<
                 unsafe extern "C" fn(
                     arg1: *const root::DB::EngineStoreServerWrap,
                     arg2: root::DB::BaseBuffView,
                 ) -> root::DB::CppStrWithView,
             >,
-            pub fn_handle_scan_page: ::std::option::Option<
+            pub fn_ps_handle_scan_page: ::std::option::Option<
                 unsafe extern "C" fn(
                     arg1: *const root::DB::EngineStoreServerWrap,
                     arg2: root::DB::BaseBuffView,
                     arg3: root::DB::BaseBuffView,
                 ) -> root::DB::RawCppPtrCarr,
             >,
-            pub fn_handle_get_lower_bound: ::std::option::Option<
+            pub fn_ps_handle_get_lower_bound: ::std::option::Option<
                 unsafe extern "C" fn(
                     arg1: *const root::DB::EngineStoreServerWrap,
                     arg2: root::DB::BaseBuffView,
                 ) -> root::DB::CppStrWithView,
             >,
-            pub fn_is_ps_empty: ::std::option::Option<
+            pub fn_ps_is_ps_empty: ::std::option::Option<
                 unsafe extern "C" fn(arg1: *const root::DB::EngineStoreServerWrap) -> u8,
             >,
-            pub fn_handle_purge_ps: ::std::option::Option<
+            pub fn_ps_handle_purge_ps: ::std::option::Option<
                 unsafe extern "C" fn(arg1: *const root::DB::EngineStoreServerWrap),
             >,
             pub fn_atomic_update_proxy: ::std::option::Option<
@@ -580,7 +580,7 @@ pub mod root {
                 ) -> root::DB::FastAddPeerRes,
             >,
         }
-        pub const RAFT_STORE_PROXY_VERSION: u64 = 16083567146900229320;
+        pub const RAFT_STORE_PROXY_VERSION: u64 = 409362199862712752;
         pub const RAFT_STORE_PROXY_MAGIC_NUMBER: u32 = 324508639;
     }
 }
