3c661f48c3bdede0b0774ec47f3fba9c11464bb0
CalvinNeo <calvinneo1995@gmail.com>
2023-09-18 14:34:26 +0800

    f
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

--- proxy_scripts/ci_check.sh
+++ proxy_scripts/ci_check.sh
@@ -40,7 +40,7 @@ elif [[ $M == "testnew" ]]; then
     cargo test --package proxy_tests --features="$ENABLE_FEATURES" --test proxy shared::write
     cargo test --package proxy_tests --features="$ENABLE_FEATURES" --test proxy shared::snapshot
     cargo test --package proxy_tests --features="$ENABLE_FEATURES" --test proxy shared::normal::store
-    cargo test --package proxy_tests --features="$ENABLE_FEATURES" --test proxy shared::normal::config
+    cargo test --package proxy_tests --features="$ENABLE_FEATURES" --test proxy shared::config
     cargo test --package proxy_tests --features="$ENABLE_FEATURES" --test proxy shared::normal::restart
     cargo test --package proxy_tests --features="$ENABLE_FEATURES" --test proxy shared::normal::persist
     cargo test --package proxy_tests --features="$ENABLE_FEATURES" --test proxy shared::ingest
