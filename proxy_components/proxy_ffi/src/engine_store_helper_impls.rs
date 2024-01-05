4f433909bca8c71e186829b983e9d5bc2847fcd6
CalvinNeo <calvinneo1995@gmail.com>
2024-01-05 12:21:39 +0800

    fmt
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

--- proxy_components/proxy_ffi/src/engine_store_helper_impls.rs
+++ proxy_components/proxy_ffi/src/engine_store_helper_impls.rs
@@ -222,10 +222,14 @@ impl EngineStoreServerHelper {
 
     pub fn apply_fap_snapshot(&self, region_id: u64, peer_id: u64, assert_exist: bool) -> bool {
         debug_assert!(self.fn_apply_fap_snapshot.is_some());
-        unsafe { (self.fn_apply_fap_snapshot.into_inner())(
-            self.inner, region_id, peer_id, 
-            if assert_exist {1} else {0}
-        ) != 0 }
+        unsafe {
+            (self.fn_apply_fap_snapshot.into_inner())(
+                self.inner,
+                region_id,
+                peer_id,
+                if assert_exist { 1 } else { 0 },
+            ) != 0
+        }
     }
 
     pub fn handle_ingest_sst(
