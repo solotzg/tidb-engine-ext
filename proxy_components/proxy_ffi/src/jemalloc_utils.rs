88ff7b3b18afecf6c41e007ac0528b93fa289756
CalvinNeo <calvinneo1995@gmail.com>
2024-07-23 13:04:26 +0800

    Revert "Enable heap profiling (#376)"
    This reverts commit 87b668286174b152df0f7ef4b738a5f46857eeff.

--- proxy_components/proxy_ffi/src/jemalloc_utils.rs
+++ proxy_components/proxy_ffi/src/jemalloc_utils.rs
@@ -23,16 +23,13 @@ extern "C" {
 #[allow(unused_variables)]
 #[allow(unused_mut)]
 #[allow(unused_unsafe)]
-pub fn issue_mallctl_args(
-    command: &str,
-    oldptr: *mut ::std::os::raw::c_void,
-    oldsize: *mut u64,
-    newptr: *mut ::std::os::raw::c_void,
-    newsize: u64,
-) {
+fn issue_mallctl(command: &str) -> u64 {
+    type PtrUnderlying = u64;
+    let mut ptr: PtrUnderlying = 0;
+    let mut size = std::mem::size_of::<PtrUnderlying>() as u64;
+    let c_str = std::ffi::CString::new(command).unwrap();
+    let c_ptr: *const ::std::os::raw::c_char = c_str.as_ptr() as *const ::std::os::raw::c_char;
     unsafe {
-        let c_str = std::ffi::CString::new(command).unwrap();
-        let c_ptr: *const ::std::os::raw::c_char = c_str.as_ptr() as *const ::std::os::raw::c_char;
         // See unprefixed_malloc_on_supported_platforms in tikv-jemalloc-sys.
         #[cfg(any(test, feature = "testexport"))]
         {
@@ -40,13 +37,25 @@ pub fn issue_mallctl_args(
             {
                 // See NO_UNPREFIXED_MALLOC
                 #[cfg(any(target_os = "android", target_os = "dragonfly", target_os = "macos"))]
-                _rjem_mallctl(c_ptr, oldptr, oldsize, newptr, newsize);
+                _rjem_mallctl(
+                    c_ptr,
+                    &mut ptr as *mut _ as *mut ::std::os::raw::c_void,
+                    &mut size as *mut u64,
+                    std::ptr::null_mut(),
+                    0,
+                );
                 #[cfg(not(any(
                     target_os = "android",
                     target_os = "dragonfly",
                     target_os = "macos"
                 )))]
-                mallctl(c_ptr, oldptr, oldsize, newptr, newsize);
+                mallctl(
+                    c_ptr,
+                    &mut ptr as *mut _ as *mut ::std::os::raw::c_void,
+                    &mut size as *mut u64,
+                    std::ptr::null_mut(),
+                    0,
+                );
             }
         }
 
@@ -54,25 +63,15 @@ pub fn issue_mallctl_args(
         {
             // Must linked to tiflash.
             #[cfg(feature = "external-jemalloc")]
-            let r = mallctl(c_ptr, oldptr, oldsize, newptr, newsize);
+            mallctl(
+                c_ptr,
+                &mut ptr as *mut _ as *mut ::std::os::raw::c_void,
+                &mut size as *mut u64,
+                std::ptr::null_mut(),
+                0,
+            );
         }
     }
-}
-
-#[allow(unused_variables)]
-#[allow(unused_mut)]
-#[allow(unused_unsafe)]
-fn issue_mallctl(command: &str) -> u64 {
-    type PtrUnderlying = u64;
-    let mut ptr: PtrUnderlying = 0;
-    let mut size = std::mem::size_of::<PtrUnderlying>() as u64;
-    issue_mallctl_args(
-        command,
-        &mut ptr as *mut _ as *mut ::std::os::raw::c_void,
-        &mut size as *mut u64,
-        std::ptr::null_mut(),
-        0,
-    );
     ptr
 }
 
