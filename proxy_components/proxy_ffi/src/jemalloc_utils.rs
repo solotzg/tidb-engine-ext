01739b338cd802d8b58b97b81a42bb4b903a54c1
CalvinNeo <calvinneo1995@gmail.com>
2024-08-09 22:25:21 +0800

    f
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

--- proxy_components/proxy_ffi/src/jemalloc_utils.rs
+++ proxy_components/proxy_ffi/src/jemalloc_utils.rs
@@ -63,7 +63,28 @@ pub fn issue_mallctl_args(
             #[cfg(not(feature = "external-jemalloc"))]
             {
                 // Happens only with `raftstore-proxy-main`
-                return mallctl(c_ptr, oldptr, oldsize, newptr, newsize);
+                #[cfg(not(any(test, feature = "testexport")))]
+                {
+                    // No test part
+                    #[cfg(feature = "external-jemalloc")]
+                    {
+                        // Must linked to tiflash.
+                        return mallctl(c_ptr, oldptr, oldsize, newptr, newsize);
+                    }
+                    #[cfg(not(feature = "external-jemalloc"))]
+                    {
+                        // Happens only with `raftstore-proxy-main`
+                        #[cfg(not(any(
+                            target_os = "android",
+                            target_os = "dragonfly",
+                            target_os = "macos"
+                        )))]
+                        {
+                            return mallctl(c_ptr, oldptr, oldsize, newptr, newsize);
+                        }
+                        0
+                    }
+                }
             }
         }
     }
